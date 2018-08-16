b7b499a98399e81d2928fd2e78b2cb740796c3b2
jon-lau <jlau@revacomm.local>
2018-08-16 09:18:40 -1000

    init repo

---
+++ Module/KalturaPlayer/config.js
@@ -0,0 +1,28 @@
+
+
+/**
+ * Created by Khoi on 7/25/2016.
+ */
+
+/********************* FOR CTOOLS MODE *********************/
+
+/*
+var deploy_env = "ctools";
+var ctool_iframe_class = "portletMainIframe";
+*/
+
+/********************* FOR INDEPENDENT MODE *********************/
+
+
+var deploy_env = "";
+
+
+/********************* FOR TEST MODE *********************/
+//var deploy_env = "test";
+//var test_iframe_class = "portletMainIframe";
+
+
+/*
+var popupWidth = 850;
+var popupHeight = 500;
+*/
